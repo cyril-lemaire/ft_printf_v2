a24619cc03bfda559d1cb4356149e447a14781cd
cyril-lemaire <cyril.lemaire@wanadoo.fr>
2018-10-31 13:11:23 +0100

    commit 3'

--- ft_writers_int_2.c
+++ ft_writers_int_2.c
@@ -14,13 +14,6 @@ int			ft_write_b(t_printer *printer)
 	return (ft_write_nondecimal(printer, "01"));
 }
 
-int			ft_write_o(t_printer *printer)
-{
-	if (printer->flags.hash)
-		printer->write(printer, "0", 1);
-	return (ft_write_nondecimal(printer, "012345678"));
-}
-
 int			ft_write_x(t_printer *printer)
 {
 	if (printer->flags.hash)
@@ -33,4 +26,11 @@ int			ft_write_X(t_printer *printer)
 	if (printer->flags.hash)
 		printer->write(printer, "0X", 2);
 	return (ft_write_nondecimal(printer, "0123456789ABCDEF"));
+}
+
+int			ft_write_o(t_printer *printer)
+{
+	if (printer->flags.hash)
+		printer->write(printer, "O", 1);
+	return (ft_write_nondecimal(printer, "012345678"));
 }
